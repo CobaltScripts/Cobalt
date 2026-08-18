819faad4c8dfd55fc1163387cd234da70d1f8b54
oblongboot <81798709+oblongboot@users.noreply.github.com>
2026-08-18 11:56:29 +0100

    chore: small cleanup velocityfailsafe

--- src/main/kotlin/org/cobalt/module/impl/failsafes/VelocityFailsafe.kt
+++ src/main/kotlin/org/cobalt/module/impl/failsafes/VelocityFailsafe.kt
@@ -1,6 +1,7 @@
 package org.cobalt.module.impl.failsafes
 
 import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket
+import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.phys.Vec3
 import org.cobalt.Cobalt
 import org.cobalt.event.annotation.SubscribeEvent
@@ -10,32 +11,32 @@ import org.cobalt.util.client.PlayerUtils
 import org.cobalt.util.failsafe.FailsafeManager
 
 object VelocityFailsafe: Failsafe("Velocity", 10, true) {
+
+  private val bouncePadVelocities = setOf(
+    1.4999694805591162,
+    2.1499725325032046,
+    2.0,
+    2.6000732466581207,
+    1.3500579869376792,
+    1.7000549349935907,
+    2.09998168833547,
+  )
+
   @SubscribeEvent
   fun onVelo(event: PacketEvent.Receive) {
-    if (Cobalt.minecraft.player == null) return
-    if (event.packet !is ClientboundSetEntityMotionPacket) return
-    if (event.packet.id != Cobalt.minecraft.player!!.id) return
+    val player = Cobalt.minecraft.player ?: return
     val packet = event.packet
-    val packetvelo = event.packet.movement
 
-    val bouncePadVelocities = listOf<Double>(
-      1.4999694805591162,
-      2.1499725325032046,
-      2.0,
-      2.6000732466581207,
-      1.3500579869376792,
-      1.7000549349935907,
-      2.09998168833547,
-    )
+    if (packet !is ClientboundSetEntityMotionPacket) return
+    if (packet.id != player.id) return
 
-    if (packetvelo == Vec3(0.0,0.0,0.0)) return
-
+    val velocity = packet.movement
+    if (velocity == Vec3.ZERO) return
     val state = minecraft.level?.getBlockState(PlayerUtils.blockStandingOn) ?: return
-    val blockUnderName = state.block.name.string
 
-    if (blockUnderName == "Slime Block" && packetvelo.y in bouncePadVelocities) return
+    if (state.block == Blocks.SLIME_BLOCK && velocity.y in bouncePadVelocities) return
 
-    FailsafeManager.alertUser(this, "VELOCITY CHANGE: $packetvelo")
+    FailsafeManager.alertUser(this, "VELOCITY CHANGE: $velocity")
   }
 
 
